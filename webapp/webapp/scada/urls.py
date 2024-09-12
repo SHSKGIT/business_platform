0c4c51e1640380fd666229f3e448ef67517d6126
Jerry Wang <jerrywang@Jerrys-Mac-mini.local>
2024-09-12 17:54:41 -0600

    add password reset

--- webapp/webapp/scada/urls.py
+++ webapp/webapp/scada/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import home, contact, subscribe, sign_in, sign_up, generate_pdf_report
+from .views import (
+    home,
+    contact,
+    subscribe,
+    sign_in,
+    sign_up,
+    generate_pdf_report,
+    reset_password,
+)
 
 app_name = "scada"
 
@@ -9,6 +17,11 @@ urlpatterns = [
     path("subscribe/", subscribe.SubscribeView.as_view(), name="subscribe"),
     path("sign-in/", sign_in.SignInView.as_view(), name="sign-in"),
     path("sign-up/", sign_up.SignUpView.as_view(), name="sign-up"),
+    path(
+        "reset-password/",
+        reset_password.ResetPasswordView.as_view(),
+        name="reset-password",
+    ),
     path(
         "pdf-report-1/", generate_pdf_report.ReportView.as_view(), name="pdf-report-1"
     ),
