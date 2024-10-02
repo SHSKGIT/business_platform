e49408dd1c83882df47960c3c8c3d26c6ab46153
Jerry Wang <jerrywang@Jerrys-Mac-mini.local>
2024-10-02 14:52:16 -0600

    update sign-up sign-in forms

--- webapp/webapp/scada/forms/sign_up.py
+++ webapp/webapp/scada/forms/sign_up.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 from .custom_fields import CustomCharField
+
+import re
 
 
 class SignUpForm(forms.Form):
@@ -12,7 +16,7 @@ class SignUpForm(forms.Form):
             attrs={
                 "id": "username",
                 "class": "span12",
-                "placeholder": "* Username (Note: MaRk = mark)",
+                "placeholder": "* Username",
                 "type": "text",
             }
         ),
@@ -41,6 +45,25 @@ class SignUpForm(forms.Form):
             "min_length": "Please enter at least 1 character.",
         },
     )
+    confirm_password = CustomCharField(
+        label="Please confirm your password",
+        # max_length=200,
+        min_length=1,
+        required=True,
+        widget=forms.PasswordInput(  # ensure the password is hidden as the user types
+            attrs={
+                "id": "confirm_password",
+                "class": "span12",
+                "placeholder": "* Confirm Password",
+                "type": "password",
+            }
+        ),
+        error_messages={
+            "required": "Please enter your confirmed password.",
+            "max_length": "Please enter no more than 200 characters.",
+            "min_length": "Please enter at least 1 character.",
+        },
+    )
     email = forms.EmailField(
         label="Please enter your email",
         max_length=200,
@@ -60,3 +83,98 @@ class SignUpForm(forms.Form):
             "min_length": "Please enter at least 1 character.",
         },
     )
+    first_name = CustomCharField(
+        label="Please enter your first name",
+        max_length=200,
+        min_length=1,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "id": "first_name",
+                "class": "span12",
+                "placeholder": "* First Name",
+                "type": "text",
+            }
+        ),
+        error_messages={
+            "required": "Please enter your first name.",
+            "max_length": "Please enter no more than 200 characters.",
+            "min_length": "Please enter at least 1 character.",
+        },
+    )
+    last_name = CustomCharField(
+        label="Please enter your last name",
+        max_length=200,
+        min_length=1,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "id": "last_name",
+                "class": "span12",
+                "placeholder": "* Last Name",
+                "type": "text",
+            }
+        ),
+        error_messages={
+            "required": "Please enter your last name.",
+            "max_length": "Please enter no more than 200 characters.",
+            "min_length": "Please enter at least 1 character.",
+        },
+    )
+    company = CustomCharField(
+        label="Please enter your company name",
+        max_length=200,
+        min_length=1,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "id": "company",
+                "class": "span12",
+                "placeholder": "* Company Name",
+                "type": "text",
+            }
+        ),
+        error_messages={
+            "required": "Please enter your company name.",
+            "max_length": "Please enter no more than 200 characters.",
+            "min_length": "Please enter at least 1 character.",
+        },
+    )
+    phone = CustomCharField(
+        label="Please enter your phone number",
+        max_length=15,
+        min_length=1,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "id": "phone",
+                "class": "span12",
+                "placeholder": "* Phone Number",
+                "type": "text",
+            }
+        ),
+        error_messages={
+            "required": "Please enter a valid phone number.",
+            "invalid": "Enter a valid phone number.",
+        },
+    )
+
+    """
+    # In Django forms, the clean_<fieldname>() method is part of Django's form validation system. When you define a method with the name clean_<fieldname>(), Django automatically recognizes it as the validation function for that specific field and calls it during form validation.
+    def clean_phone(self):
+        phone = self.cleaned_data.get("phone")
+
+        # Strip spaces, dashes, and parentheses
+        stripped_phone = re.sub(r"[\s()-]", "", phone)
+
+        # Check if the phone starts with a plus and contains only digits afterwards
+        if not re.match(r"^\+?\d{9,15}$", stripped_phone):
+            raise ValidationError(
+                "Phone number must be between 9 and 15 digits and can include an optional '+'."
+            )
+
+        # Further validation logic (optional)
+        # For example, you can check country codes, or local formatting rules if needed
+
+        return phone
+    """
--- webapp/webapp/scada/views/sign_up.py
+++ webapp/webapp/scada/views/sign_up.py
@@ -12,6 +12,16 @@ from ..forms.contact import ContactForm
 from ..forms.subscribe import SubscribeForm
 from ..forms.sign_up import SignUpForm
 
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+ADMIN_EMAIL = [
+    env("ADMIN_EMAIL_1"),
+]
+
 
 # =======================================================================================================================
 class SignUpView(View):
@@ -29,14 +39,20 @@ class SignUpView(View):
         pass
         sign_up_form = SignUpForm(request.POST)
         if sign_up_form.is_valid():
-            username = sign_up_form.cleaned_data["username"].lower()
+            username = sign_up_form.cleaned_data["username"]
             password = sign_up_form.cleaned_data["password"]
+            first_name = sign_up_form.cleaned_data["first_name"]
+            last_name = sign_up_form.cleaned_data["last_name"]
+            company = sign_up_form.cleaned_data["company"]
+            phone = sign_up_form.cleaned_data["phone"]
             email = sign_up_form.cleaned_data["email"].lower()
 
             # check user
             dbsession = next(get_dbsession())  # Get the SQLAlchemy session
             user = (
-                dbsession.query(AuthEntity).filter_by(username=username).one_or_none()
+                dbsession.query(AuthEntity)
+                .filter_by(username=username, email=email)
+                .one_or_none()
             )
 
             if user:
@@ -46,23 +62,31 @@ class SignUpView(View):
                 return JsonResponse(
                     {
                         "success": False,
-                        "sign_up_form_invalid_error": "Username exists, try others.",
+                        "sign_up_form_invalid_error": "Username with email exists, try others.",
                     }
                 )
-            else:
-                # create a new user
-                new_user = AuthEntity(username=username, email=email)
-                new_user.set_password(password)
-                dbsession.add(new_user)
-                dbsession.commit()
-                dbsession.refresh(new_user)
-                dbsession.close()
 
-                return JsonResponse(
-                    {
-                        "success": True,
-                    }
-                )
+            # create a new user
+            new_user = AuthEntity(
+                username=username,
+                first_name=first_name,
+                last_name=last_name,
+                company=company,
+                phone=phone,
+                email=email,
+                is_admin=SignUpView.is_admin(email),
+            )
+            new_user.set_password(password)
+            dbsession.add(new_user)
+            dbsession.commit()
+            dbsession.refresh(new_user)
+            dbsession.close()
+
+            return JsonResponse(
+                {
+                    "success": True,
+                }
+            )
         else:
             # If the form is not valid, render the form with errors
             template = "scada/sign_up.html"
@@ -71,3 +95,10 @@ class SignUpView(View):
                 "sign_up_form": sign_up_form,
             }
             return render(request, template, context)
+
+    @staticmethod
+    def is_admin(email):
+        if email in ADMIN_EMAIL:
+            return True
+        else:
+            return False
